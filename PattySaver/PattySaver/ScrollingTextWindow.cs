2a2ae35ffcfc6401ae8b7cd20d200af649643d38
JK <JKDev@anonimail.net>
2014-09-13 00:31:32 -0700

    Again.

--- PattySaver/PattySaver/ScrollingTextWindow.cs
+++ PattySaver/PattySaver/ScrollingTextWindow.cs
@@ -9,7 +9,6 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 using ScotSoft.PattySaver;
-using ScotSoft.PattySaver.LaunchManager;
 using ScotSoft.PattySaver.DebugUtils;
 
 namespace ScotSoft.PattySaver
